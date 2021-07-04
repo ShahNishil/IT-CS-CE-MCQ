b96423405cb2b49f166622d17c177cf606a74fd8
ShahNishil <shahnishil937@gmail.com>
2021-07-05 00:49:39 +0530

    17 and 18 Video

---
+++ app/src/main/java/com/nbs/it_cs_ce_mcq/QuestionsAdapter.java
@@ -0,0 +1,42 @@
+package com.nbs.it_cs_ce_mcq;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class QuestionsAdapter extends RecyclerView.Adapter<QuestionsAdapter.ViewHolder> {
+
+    private List<QuestionModel> questionsList;
+
+    public QuestionsAdapter(List<QuestionModel> questionsList) {
+        this.questionsList = questionsList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view= LayoutInflater.from(viewGroup.getContext()).inflate()
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuestionsAdapter.ViewHolder viewholder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
