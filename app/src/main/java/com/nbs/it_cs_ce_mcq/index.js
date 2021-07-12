152d9141025796bbd2350f37597eae9ed67a7e9c
BrijeshSukhadiya <brisuk1234@gmail.com>
2021-07-12 11:16:23 +0530

    JS

---
+++ app/src/main/java/com/nbs/it_cs_ce_mcq/index.js
@@ -0,0 +1,33 @@
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+apiKey: "AIzaSyB5GiFIrm4q9Q3s2LRlXdamiHleCXYb9hM",
+authDomain: "<it-cs-ce-mcqs.firebaseapp.com>",
+projectId: "it-cs-ce-mcqs"
+});
+
+var db = firebase.firestore();
+
+var menu =[
+{
+"NAME":"vb.net",
+"NUMBER_OF_TESTS":4
+},
+{
+"NAME":"javascript",
+"NUMBER_OF_TESTS":4
+}
+]
+
+menu.forEach(function(obj) {
+db.collection("QUIZ").add({
+NAME: obj.NAME,
+NUMBER_OF_TESTS:obj.NUMBER_OF_TESTS,
+}).then(function(docRef) {
+console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+console.error("Error adding document: ", error);
+});
+});
