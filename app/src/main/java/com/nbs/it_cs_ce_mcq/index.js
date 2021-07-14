5a65653f333ee052705a982abce512abd51f2a58
Bhavesh <bhavnanibhavesh14@gmail.com>
2021-07-14 09:45:09 +0530

    added the json data of test 3 and 4 in the index.js file.

--- app/src/main/java/com/nbs/it_cs_ce_mcq/index.js
+++ app/src/main/java/com/nbs/it_cs_ce_mcq/index.js
@@ -281,6 +281,345 @@ switch(a)
 "C":"continue",
 "D":"return",
 "ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":" What will be the output of the following Java program?
+
+class Output
+{
+        public static void main(String args[])
+        {
+           final int a=10,b=20;
+          while(a<b)
+          {
+
+          System.out.println("Hello");
+          }
+          System.out.println("World");
+
+        }
+}",
+"A":"Hello",
+"B":"run time error",
+"C":"Hello world",
+"D":"compilation error",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"The while loop repeats a set of code while the condition is not met?",
+"A":"true",
+"B":"false",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"Choose a valid loop name in Java below.",
+"A":"for",
+"B":"while",
+"C":"do-while",
+"D":"all of the above",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"Every loop in Java has a condition that should be ___ in order to proceed for execution. (TRUE / FALSE)",
+"A":"true",
+"B":"false",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"Choose the correct syntax of the WHILE loop in Java below.",
+"A":"while(condition)
+{
+  //statements
+}",
+"B":"while(condition);
+{
+  //statements
+}",
+"C":"while
+{
+  //statements
+}(condition)",
+"D":" None",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"A CONTINUE statement inside a Loop like WHILE, FOR, DO-WHILE and Enhanced-FOR causes the program execution ___ the loop.",
+"A":"skip",
+"B":"Skip present iteration and continue with next iteration of the loop",
+"C":"exit",
+"D":"none of the above",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"Which of the following loops will execute the body of loop even when condition controlling the loop is initially false?",
+"A":"do-while",
+"B":"while",
+"C":"for",
+"D":"none of the above",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"What will be the output of the following Java program?
+
+    class comma_operator
+    {
+        public static void main(String args[])
+        {
+             int sum = 0;
+             for (int i = 0, j = 0; i < 5 & j < 5; ++i, j = i + 1)
+                 sum += i;
+ 	     System.out.println(sum);
+        }
+    }",
+"A":"5",
+"B":"6",
+"C":"14",
+"D":"compilation error",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"What will be the output of the following Java program?
+
+    class jump_statments
+    {
+        public static void main(String args[])
+        {
+             int x = 2;
+             int y = 0;
+             for ( ; y < 10; ++y)
+             {
+                 if (y % x == 0)
+                     continue;
+                 else if (y == 8)
+                      break;
+                 else
+                    System.out.print(y + " ");
+             }
+        }
+    }",
+"A":"1 3 5 7",
+"B":"2 4 6 8",
+"C":"1 3 5 7 9",
+"D":"1 2 3 4 5 6 7 8 9",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST3",
+"QUESTION":"Choose the Java-Code below with a never-ending loop.",
+"A":"while(true);",
+"B":"for(;true;);",
+"C":"do
+     {
+       ;
+     }while(true);",
+"D":"all of the above",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"Which of these class contains the methods used to write in a file?",
+"A":"FileStream",
+"B":"FileInputStream",
+"C":"BUfferedOutputStream",
+"D":"FileBufferStream",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"Which of these methods are used to read in from file?",
+"A":" get()",
+"B":" read()",
+"C":"scan()",
+"D":"readFileInput()",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"Which of these exception is thrown in cases when the file specified for writing is not found?",
+"A":" IOException",
+"B":"FileException",
+"C":"FileNotFoundException",
+"D":"FileInputException",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":" Which of these values is returned by read() method is end of file (EOF) is encountered?",
+"A":"0",
+"B":"1",
+"C":"-1",
+"D":"null",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":" Which of these methods is used to write() into a file?",
+"A":"put()",
+"B":"putfile()",
+"C":"write()",
+"D":"writefile()",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"What will be the output of the following Java program?
+
+                import java.io.*;
+                class filesinputoutput
+                {
+                    public static void main(String args[])
+                    {
+                        InputStream obj = new FileInputStream("inputoutput.java");
+                        System.out.print(obj.available());
+                    }
+                }",
+"A":" true",
+"B":" false",
+"C":" prints number of bytes in file",
+"D":"prints number of characters in the file",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"",
+"A":"",
+"B":"",
+"C":"",
+"D":"",
+"ANSWER":,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"What will be the output of the following Java program?
+
+                import java.io.*;
+                class Chararrayinput
+                {
+                    public static void main(String[] args)
+                    {
+            	    String obj  = "abcdefgh";
+                        int length = obj.length();
+                        char c[] = new char[length];
+                        obj.getChars(0, length, c, 0);
+                        CharArrayReader input1 = new CharArrayReader(c);
+                        CharArrayReader input2 = new CharArrayReader(c, 1, 4);
+                        int i;
+                        int j;
+                        try
+                        {
+            		while((i = input1.read()) == (j = input2.read()))
+                            {
+                                System.out.print((char)i);
+                            }
+                   	    }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+            	    }
+            	}
+                }",
+"A":"abc",
+"B":"abcd",
+"C":"abcde",
+"D":"none of the above",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"What will be the output of the following Java program?
+
+                import java.io.*;
+                class Chararrayinput
+                {
+                    public static void main(String[] args)
+                    {
+            	    String obj  = "abcdef";
+                        int length = obj.length();
+                        char c[] = new char[length];
+                        obj.getChars(0, length, c, 0);
+                        CharArrayReader input1 = new CharArrayReader(c);
+                        CharArrayReader input2 = new CharArrayReader(c, 0, 3);
+                        int i;
+                        try
+                        {
+            		while((i = input2.read()) != -1)
+                            {
+                                System.out.print((char)i);
+                            }
+                   	    }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+            	    }
+            	}
+                }",
+"A":"abc",
+"B":"abcd",
+"C":"abcde",
+"D":"abcdef",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST4",
+"QUESTION":"What will be the output of the following Java program?
+
+                import java.io.*;
+                public class filesinputoutput
+                {
+                	public static void main(String[] args)
+                    {
+             	   String obj  = "abc";
+                       byte b[] = obj.getBytes();
+                       ByteArrayInputStream obj1 = new ByteArrayInputStream(b);
+                       for (int i = 0; i < 2; ++ i)
+                       {
+                           int c;
+                           while((c = obj1.read()) != -1)
+                           {
+                        	   if(i == 0)
+                               {
+                        	       System.out.print(Character.toUpperCase((char)c));
+                                   obj2.write(1);
+                        	   }
+                           }
+                           System.out.print(obj2);
+                       }
+                    }
+                }",
+"A":"AaBaCa",
+"B":" ABCaaa",
+"C":"AaaBaaCaa",
+"D":"AaBaaCaaa",
+"ANSWER":4,
 }
 ]
 
