8c358e3cc2d5dbd774cdd8c8997eca57b4a65b5c
Bhavesh <bhavnanibhavesh14@gmail.com>
2021-07-15 09:47:53 +0530

    added the json data for java test 5,6 and 7

--- app/src/main/java/com/nbs/it_cs_ce_mcq/index.js
+++ app/src/main/java/com/nbs/it_cs_ce_mcq/index.js
@@ -620,6 +620,337 @@ class Output
 "C":"AaaBaaCaa",
 "D":"AaBaaCaaa",
 "ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":" What is the range of short data type in Java?",
+"A":"-128 to 127",
+"B":"-32768 to 32767",
+"C":"-2147483648 to 2147483647",
+"D":"None of the mentioned",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":"What is the range of byte data type in Java?",
+"A":"-128 to 127",
+"B":"-32768 to 32767",
+"C":"-2147483648 to 2147483647",
+"D":"None of the mentioned",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":" Which of the following are legal lines of Java code?
+   1. int w = (int)888.8;
+   2. byte x = (byte)100L;
+   3. long y = (byte)100;
+   4. byte z = (byte)100L;
+",
+"A":" 1 and 2 ",
+"B":"2 and 3",
+"C":" 3 and 4",
+"D":"All statements are correct",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":"An expression involving byte, int, and literal numbers is promoted to which of these?",
+"A":"int",
+"B":"long",
+"C":"byte",
+"D":"float",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":" Which of these literals can be contained in float data type variable?",
+"A":" -1.7e+308",
+"B":"-3.4e+038",
+"C":" +1.7e+308",
+"D":"-3.4e+050",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":" Which data type value is returned by all transcendental math functions?",
+"A":" int",
+"B":"float",
+"C":"double",
+"D":"long",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":"Which of these coding types is used for data type characters in Java?",
+"A":" ASCII",
+"B":"ISO-LATIN-1",
+"C":"UNICODE",
+"D":" None of the mentioned",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":"Which of these values can a boolean variable contain?",
+"A":"True & False",
+"B":" 0 & 1",
+"C":"Any integer value",
+"D":"true",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":" Which one is a valid declaration of a boolean?",
+"A":" boolean b1 = 1;",
+"B":" boolean b2 = ‘false’;",
+"C":" boolean b3 = false;",
+"D":" boolean b4 = ‘true’",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST5",
+"QUESTION":" What will be the output of the following Java code?
+
+    class booloperators {
+        public static void main(String args[])
+        {
+            boolean var1 = true;
+	    boolean var2 = false;
+	    System.out.println((var1 & var2));
+        }
+    }",
+"A":"0",
+"B":"1",
+"C":"true",
+"D":"false",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":" Which of these operators is used to allocate memory to array variable in Java?",
+"A":"malloc",
+"B":"alloc",
+"C":"new",
+"D":"new malloc",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":"Which of these is an incorrect array declaration?",
+"A":" int arr[] = new int[5] ",
+"B":"int [] arr = new int[5]",
+"C":"int arr[] = new int[5]",
+"D":"int arr[] = int [5] new",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":"What will be the output of the following Java code?
+    int arr[] = new int [5];
+    System.out.print(arr);",
+"A":"0 ",
+"B":"value stored in arr[0]",
+"C":"00000",
+"D":"Class name@ hashcode in hexadecimal form",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":"Which of these is an incorrect Statement?",
+"A":"It is necessary to use new operator to initialize an array",
+"B":"Array can be initialized using comma separated expressions surrounded by curly braces",
+"C":"Array can be initialized when they are declared",
+"D":"None of the mentioned",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":" What will be the output of the following Java code?
+
+                class array_output
+                {
+                    public static void main(String args[])
+                    {
+                        int array_variable [] = new int[10];
+            	    for (int i = 0; i < 10; ++i)
+                        {
+                            array_variable[i] = i;
+                            System.out.print(array_variable[i] + " ");
+                            i++;
+                        }
+                    }
+                }",
+"A":" 0 2 4 6 8 ",
+"B":" 1 3 5 7 9",
+"C":"0 1 2 3 4 5 6 7 8 9",
+"D":"1 2 3 4 5 6 7 8 9 10",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":" What is the type of variable ‘b’ and ‘d’ in the following Java snippet?
+
+            int a[], b;
+            int []c, d;",
+"A":" ‘b’ and ‘d’ are int  ",
+"B":"‘b’ and ‘d’ are arrays of type int",
+"C":" ‘b’ is int variable; ‘d’ is int array",
+"D":"‘d’ is int variable; ‘b’ is int array",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":"Generics does not work with?",
+"A":"Set",
+"B":"List",
+"C":"Tree",
+"D":"Array",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":"How to sort an array?",
+"A":"Array.sort() ",
+"B":"Arrays.sort()",
+"C":"Collection.sort()",
+"D":"System.sort()",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":"How to copy contents of array?",
+"A":" System.arrayCopy()  ",
+"B":"Array.copy()",
+"C":"Arrays.copy()",
+"D":"Collection.copy()",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST6",
+"QUESTION":"Can you make an array volatile?",
+"A":"true ",
+"B":"false",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":" Which of the following is not OOPS concept in Java?",
+"A":"inheritance",
+"B":"polymorphism",
+"C":"encapsulation",
+"D":"all",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":" Which of the following is a type of polymorphism in Java?",
+"A":"Compile time polymorphism ",
+"B":"Execution time polymorphism",
+"C":" Multiple polymorphism",
+"D":"Multilevel polymorphism",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":" Which concept of Java is a way of converting real world objects in terms of class?",
+"A":" Polymorphism ",
+"B":"Encapsulation",
+"C":"Abstraction",
+"D":"Inheritance",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":"Which concept of Java is achieved by combining methods and attribute into a class?",
+"A":" Encapsulation",
+"B":"Inheritance",
+"C":"Polymorphism",
+"D":" Abstraction",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":"What is it called if an object has its own lifecycle and there is no owner?",
+"A":"Aggregation",
+"B":"Composition",
+"C":"Encapsulation",
+"D":"Association",
+"ANSWER":4,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":"What is it called where child object gets killed if parent object is killed?",
+"A":"Aggregation",
+"B":"Composition",
+"C":"Encapsulation",
+"D":"Associatio",
+"ANSWER":2,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":"What is it called where object has its own lifecycle and child object cannot belong to another parent object?",
+"A":"Aggregation",
+"B":"Composition",
+"C":"Encapsulation",
+"D":" Association",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":"Which of the following is not an OOPS concept?",
+"A":"Encapsulation",
+"B":"Polymorphism",
+"C":"Exception",
+"D":"Abstraction",
+"ANSWER":3,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":"Which among the following cannot be used for the concept of polymorphism?",
+"A":"Static member function",
+"B":"Constructor Overloading",
+"C":"Member function overloading",
+"D":"Global member function",
+"ANSWER":1,
+},
+{
+"CATEGORY":"QXMDYGn4BursvyhqYp7b",
+"TEST":"TEST7",
+"QUESTION":" Which function best describe the concept of polymorphism in programming languages?",
+"A":"Class member function",
+"B":"Virtual function",
+"C":"Inline function",
+"D":"Undefined function",
+"ANSWER":2,
 }
 ]
 
